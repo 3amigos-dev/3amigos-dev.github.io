9d4b035c4551eff0d3e235e8d90308ebe22d1cc5
Tim Gates <tim.gates@iress.com>
2019-06-11 19:27:52 +1000

    Basic jekyll site unchanged

--- ci/build.sh
+++ ci/build.sh
@@ -4,8 +4,15 @@ THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASEDIR="$( dirname "${THISDIR}" )"
 
 cd "${BASEDIR}"
+rm -rf "${BASEDIR}/blog/_site"
+rm -rf "${BASEDIR}/site"
 export JEKYLL_VERSION=3.7.3
 docker run --rm \
   "--mount" "type=bind,src=${BASEDIR}/blog,dst=/srv/jekyll" \
   -it jekyll/jekyll:$JEKYLL_VERSION \
   jekyll build /srv/jekyll
+mv "${BASEDIR}/blog/_site" "${BASEDIR}/site"
+if ! git status -s >/dev/null 2>&1 ; then
+    echo 'File Modifications Exist.' >&2
+    exit 1
+fi
