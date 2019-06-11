619ca81926488f0c365b3e4d2eae9ac33eda25be
Tim Gates <tim.gates@iress.com>
2019-06-11 19:15:03 +1000

    Bump jekyll for security vulnerability fix

--- ci/build.sh
+++ ci/build.sh
@@ -4,9 +4,8 @@ THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASEDIR="$( dirname "${THISDIR}" )"
 
 cd "${BASEDIR}"
-export JEKYLL_VERSION=3.5
+export JEKYLL_VERSION=3.7.3
 docker run --rm \
-  "--user" "$(id -u):$(id -g)" \
-  "--mount" "type=bind,src=${BASEDIR},dst=/srv/jekyll" \
+  "--mount" "type=bind,src=${BASEDIR}/blog,dst=/srv/jekyll" \
   -it jekyll/jekyll:$JEKYLL_VERSION \
   jekyll build /srv/jekyll
